094b638227aa235be7d0aa7224e28aa5ee2339e7
西米屋花火 <3620569989@qq.com>
2024-11-25 20:30:37 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+# ///////////////////////////////////////////////////////////////
+#
+# BY: WANDERSON M.PIMENTA
+# PROJECT MADE WITH: Qt Designer and PySide6
+# V: 1.0.0
+#
+# This project can be used freely for all uses, as long as they maintain the
+# respective credits only in the Python scripts, any information in the visual
+# interface (GUI) can be modified without any implication.
+#
+# There are limitations on Qt licenses if you want to use your products
+# commercially, I recommend reading them on the official website:
+# https://doc.qt.io/qtforpython/licenses.html
+#
+# ///////////////////////////////////////////////////////////////
+
+# IMPORT PACKAGES AND MODULES
+# ///////////////////////////////////////////////////////////////
+#from gui.uis.windows.main_window.functions_main_window import *
+import sys
+import os
+
+# IMPORT QT CORE
+# ///////////////////////////////////////////////////////////////
+from qt_core import *
+
+# IMPORT SETTINGS
+# ///////////////////////////////////////////////////////////////
+from gui.core.json_settings import Settings
+
+# IMPORT PY ONE DARK WINDOWS
+# ///////////////////////////////////////////////////////////////
+# MAIN WINDOW
+from gui.uis.windows.main_window import *
+
+from gui.uis.windows.main_window.functions_main_window import MainFunctions
+
+# IMPORT PY ONE DARK WIDGETS
+# ///////////////////////////////////////////////////////////////
+from gui.widgets import *
+
+# ADJUST QT FONT DPI FOR HIGHT SCALE AN 4K MONITOR
+# ///////////////////////////////////////////////////////////////
+os.environ["QT_FONT_DPI"] = "96"
+# IF IS 4K MONITOR ENABLE 'os.environ["QT_SCALE_FACTOR"] = "2"'
+
+# MAIN WINDOW
+# ///////////////////////////////////////////////////////////////
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # SETUP MAIN WINDOw
+        # Load widgets from "gui\uis\main_window\ui_main.py"
+        # ///////////////////////////////////////////////////////////////
+        self.ui = UI_MainWindow()
+        self.ui.setup_ui(self)
+
+        # LOAD SETTINGS
+        # ///////////////////////////////////////////////////////////////
+        settings = Settings()
+        self.settings = settings.items
+
+        # SETUP MAIN WINDOW
+        # ///////////////////////////////////////////////////////////////
+        self.hide_grips = True # Show/Hide resize grips
+        SetupMainWindow.setup_gui(self)
+        # SHOW MAIN WINDOW
+        # ///////////////////////////////////////////////////////////////
+        self.show()
+
+
+    def btn_clicked(self):
+        # GET BT CLICKED
+        btn = SetupMainWindow.setup_btns(self)
+
+        if btn.objectName() == "btn_home":
+            self.ui.left_menu.select_only_one(btn.objectName())
+            MainFunctions.set_page(self, self.ui.load_pages.page_1)
+
+        if btn.objectName() == "btn_info":
+            self.ui.left_menu.select_only_one(btn.objectName())
+            MainFunctions.set_page(self, self.ui.load_pages.page_2)
+
+
+
+
+    def btn_released(self):
+        # GET BT CLICKED
+        btn = SetupMainWindow.setup_btns(self)
+
+        # DEBUG
+        print(f"Button {btn.objectName()}, released!")
+
+    # RESIZE EVENT
+    # ///////////////////////////////////////////////////////////////
+    def resizeEvent(self, event):
+        SetupMainWindow.resize_grips(self)
+
+    # MOUSE CLICK EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        # SET DRAG POS WINDOW
+        self.dragPos = event.globalPos()
+
+
+# SETTINGS WHEN TO START
+# Set the initial class and also additional parameters of the "QApplication" class
+# ///////////////////////////////////////////////////////////////
+if __name__ == "__main__":
+    # APPLICATION
+    # ///////////////////////////////////////////////////////////////
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.ico"))
+    window = MainWindow()
+
+    # EXEC APP
+    # ///////////////////////////////////////////////////////////////
+    sys.exit(app.exec())
---
+++ script_utils.py
@@ -0,0 +1,44 @@
+from DrissionPage import Chromium, ChromiumOptions
+from time import sleep
+
+
+def init_browser(exe_path, port = 9999):
+    chromium_options = ChromiumOptions().set_browser_path(exe_path)
+    chromium_options.set_local_port(port)
+    return Chromium(addr_or_opts=chromium_options)
+
+def work(browser):
+    sbtree = browser.latest_tab
+
+    chapter_container = sbtree.ele('x://*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div')
+    chapters = chapter_container.children()
+    for chapter in chapters:
+        sections = []
+        for i in chapter.children():
+            if i.tag == 'div':
+                sections.append(i)
+        for section in sections:
+            videos = section.child(2).children()
+            for video in videos:
+                video = video.child(2)
+                if video.child(3).attr('class') == 'fl time_icofinish':
+                    continue
+                video.click()
+                sleep(2)
+                while True:
+                    try:
+                        sbtree.ele('@class=speedBox').click()
+                        break
+                    except Exception as e:
+                        sleep(0.1)
+
+                sbtree.ele('@class=speedTab speedTab15').click()
+                sbtree.ele('@class=videoArea').click()
+                while (video.child(3).attr('class') != 'fl time_icofinish'):
+                    if sbtree.ele('@aria-label=弹题测验'):
+                        window = sbtree.ele('@aria-label=弹题测验')
+                        window.ele('@class=topic-item').child().click()
+                        window.ele('@class=el-dialog__footer').click()
+                        sleep(1)
+                        sbtree.ele('@class=videoArea').click()
+
