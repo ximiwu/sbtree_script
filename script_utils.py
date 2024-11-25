459a71baf4cccf6d2df48b42bcf0c879bdb24696
西米屋花火 <3620569989@qq.com>
2024-11-25 23:31:11 +0800

    Add files via upload

--- script_utils.py
+++ script_utils.py
@@ -11,39 +11,38 @@ def work(browser):
     sbtree = browser.latest_tab
 
     chapter_container = sbtree.ele('x://*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div')
-    chapters = chapter_container.children()
-    for chapter in chapters:
-        sections = []
-        for i in chapter.children():
-            if i.tag == 'div':
-                sections.append(i)
-        for section in sections:
-            videos = section.child(2).children()
-            for video in videos:
-                video = video.child(2)
-                if video.child(3).attr('class') == 'fl time_icofinish':
-                    continue
-                video.click()
-                while True:
-                    try:
-                        sbtree.ele('@class=speedBox').click()
-                        sleep(0.01)
-                    except Exception as e:
-                        break
-                while True:
-                    try:
-                        sbtree.ele('@class=speedBox').click()
-                        break
-                    except Exception as e:
-                        sleep(0.01)
+
+    videos = chapter_container.eles('@class=clearfix video')
+    videos.insert(0, chapter_container.ele('@class=clearfix video current_play'))
+    print(videos)
+
+    for video in videos:
+        video = video.ele('@class=fl cataloguediv-c')
+
+        if len(video.eles('@class=fl time_icofinish')) != 0:
+            continue
+        video.click()
+
+        while True:
+            try:
+                sbtree.ele('@class=speedBox').click()
+                sleep(0.01)
+            except Exception as e:
+                break
+        while True:
+            try:
                 sbtree.ele('@class=speedBox').hover()
-                sbtree.ele('@class=speedTab speedTab15').click()
+                break
+            except Exception as e:
+                sleep(0.01)
+        sbtree.ele('@class=speedBox').hover()
+        sbtree.ele('@class=speedTab speedTab15').click()
+        sbtree.ele('@class=videoArea').click()
+        while (len(video.eles('@class=fl time_icofinish')) == 0):
+            if sbtree.ele('@aria-label=弹题测验'):
+                window = sbtree.ele('@aria-label=弹题测验')
+                window.ele('@class=topic-item').child().click()
+                window.ele('@class=el-dialog__footer').click()
+                sleep(1)
                 sbtree.ele('@class=videoArea').click()
-                while (video.child(3).attr('class') != 'fl time_icofinish'):
-                    if sbtree.ele('@aria-label=弹题测验'):
-                        window = sbtree.ele('@aria-label=弹题测验')
-                        window.ele('@class=topic-item').child().click()
-                        window.ele('@class=el-dialog__footer').click()
-                        sleep(1)
-                        sbtree.ele('@class=videoArea').click()
 
